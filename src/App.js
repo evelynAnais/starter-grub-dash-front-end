77affe69a2e31617cb4bec00d62fb1f8d4904f99
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-07 19:30:39 -0600

    changed home path

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="app-routes">
       <Switch>
-        <Route path="/">
+        <Route path="/starter-grub-dash-front-end">
           <Layout />
         </Route>
       </Switch>
